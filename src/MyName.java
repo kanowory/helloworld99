6c1c8f3a6686647bb4f29389060b852c6be5e891
kanowory <kanowory@student.wszib.edu.pl>
2022-10-09 13:05:06 +0200

    Zdublowanie linii kodu

--- src/MyName.java
+++ src/MyName.java
@@ -2,5 +2,9 @@ public class MyName {
     public static void main(String[] args){
         String name = "Kasia";
         System.out.println("Moje imię to: " + name);
+        System.out.println("Moje imię to: " + name);
+        System.out.println("Moje imię to: " + name);
+        System.out.println("Moje imię to: " + name);
+        System.out.println("Moje imię to: " + name);
     }
 }
